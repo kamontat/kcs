ffb6caefc7adbd60e28120e688100619a5a6d459
Kamontat Chantrachirathumrong <github@kc.in.th>
2023-04-17 00:34:26 +0700

    chore(debug): add more debug information on utils _debug.sh

--- scripts/utils/_debug.sh
+++ scripts/utils/_debug.sh
@@ -6,17 +6,39 @@
 # set -n #EVALUATE - Check syntax of the script but don't execute.
 # set -e #ERROR    - Force exit if error occurred.
 
-__kcs_debug_pre_main() {
+__kcs_debug_key_value_format() {
   local ns="$KCS_HOOK_NAME info"
+  local key="$1" value="$2"
 
-  kcs_debug "$ns" "%s: '%s'" \
+  kcs_debug "$ns" "%-15s: '%s'" \
+    "$key" "$value"
+}
+
+__kcs_debug_pre_main() {
+  __kcs_debug_key_value_format \
     "$KCS_NAME" "$KCS_VERSION"
-  kcs_debug "$ns" "mode: '%s'" \
-    "$KCS_MODE"
-  kcs_debug "$ns" "entry mode: '%s'" \
-    "$_KCS_ENTRY"
-  kcs_debug "$ns" "arguments: '%s' (size=%d)" \
-    "$*" "$#"
-  kcs_debug "$ns" "log levels: '%s'" \
-    "$_KCS_LOG_LEVELS"
+  __kcs_debug_key_value_format \
+    "mode" "$KCS_MODE"
+  __kcs_debug_key_value_format \
+    "root" "$KCS_ROOT"
+
+  __kcs_debug_key_value_format \
+    "original dir" "$_KCS_DIR_ORIG"
+  __kcs_debug_key_value_format \
+    "script dir" "$_KCS_DIR_SCRIPT"
+  __kcs_debug_key_value_format \
+    "root dir" "$_KCS_DIR_ROOT"
+  __kcs_debug_key_value_format \
+    "temp dir" "$_KCS_DIR_TEMP"
+  __kcs_debug_key_value_format \
+    "logs dir" "$_KCS_DIR_LOG"
+
+  __kcs_debug_key_value_format \
+    "entry mode" "$_KCS_ENTRY"
+  __kcs_debug_key_value_format \
+    "argument size" "$#"
+  __kcs_debug_key_value_format \
+    "arguments" "$*"
+  __kcs_debug_key_value_format \
+    "log levels" "$_KCS_LOG_LEVELS"
 }
