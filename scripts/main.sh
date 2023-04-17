9376ec9a41fcf59c722d96521beb1f897776bd1c
Kamontat Chantrachirathumrong <github@kc.in.th>
2023-04-17 19:30:56 +0700

    fix: wrong mode configure in main.sh

--- scripts/main.sh
+++ scripts/main.sh
@@ -15,7 +15,7 @@ export KCS_ROOT="1"
 ## Possible values: command or library
 ## command: Scripts will create all functions and run (default)
 ## library: Scripts will create all functions, but not execute them
-export KCS_MODE="library"
+export KCS_MODE="command"
 
 ####################################################
 ## Internal function calls
