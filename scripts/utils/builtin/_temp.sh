acaa2118031b4f8c3ee283f0d535c560e552b6f6
Kamontat Chantrachirathumrong <github@kc.in.th>
2023-04-19 18:48:54 +0700

    perf(utils): add new temp apis

--- scripts/utils/builtin/_temp.sh
+++ scripts/utils/builtin/_temp.sh
@@ -6,19 +6,21 @@
 ## Requirement:
 ##   <none>
 ## Public functions:
-##   `kcs_clean_temp` - clean temp directory to initiate state
+##   `kcs_temp_create [n]` - create new temp directory by name
+##   `kcs_temp_clean <n>` - clean <n> temp directory
+##   `kcs_temp_clean_all` - clean temp directory to initiate state
 
 # set -x #DEBUG    - Display commands and their arguments as they are executed.
 # set -v #VERBOSE  - Display shell input lines as they are read.
 # set -n #EVALUATE - Check syntax of the script but don't execute.
 # set -e #ERROR    - Force exit if error occurred.
 
-## create temp directory
-## @param $1 - [optional] name (default random by date)
+## create temp directory and return fullpath
+## @param $1 - [optional] name (default=random)
 ## @return   - single line fullpath string
 kcs_temp_create() {
   local name="$1"
-  test -z "$name" && name="_TMP$(date +"%Y%m%d%H%M%S")"
+  test -z "$name" && name=".t$RANDOM"
 
   local fullpath="$_KCS_DIR_TEMP/$name"
   ## create temporary directory
@@ -26,7 +28,26 @@ kcs_temp_create() {
   printf "%s" "$fullpath"
 }
 
-kcs_clean_temp() {
+## clean input temp directory name
+## @param $1 - [required] directory name (default=random)
+## @return   - zero code if cleanup completed
+kcs_temp_clean() {
+  local ns="temp-cleaner"
+  local name="$1"
+  local fullpath="$_KCS_DIR_TEMP/$name"
+
+  if test -d "$fullpath" ||
+    test -f "$fullpath"; then
+    kcs_debug "$ns" "%s now cleaned" \
+      "$fullpath"
+
+    rm -r "$fullpath"
+  fi
+  return 1
+}
+
+## cleanup whole temp directory
+kcs_temp_clean_all() {
   local ns="temp-cleaner"
 
   if test -d "$_KCS_DIR_TEMP"; then
