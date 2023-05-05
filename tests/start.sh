5b3bc96c3a68562a84cff646b5a3b92a6d926459
Kamontat Chantrachirathumrong <github@kc.in.th>
2023-05-05 22:51:07 +0700

    feat(test): reworks testing

--- tests/start.sh
+++ tests/start.sh
@@ -1,66 +1,185 @@
 #!/usr/bin/env bash
 
 ## Possible values: 'validate', 'snapshot'
-export __TEST_MODE="${TEST_MODE:-validate}"
+# TEST_MODE=validate
 
 main() {
-  run_test \
+  test_setup
+
+  test_case \
     main logs
-  run_test \
+  test_case \
     disable debug
+
+  test_cleanup
 }
 
-## $1 - test name
-## $@ - test arguments
-run_test() {
-  local command_dir="$PWD/tests/commands"
-  local snapshot_dir="$PWD/tests/snapshots"
-  local temp_dir="$PWD/tests/.tmp"
-  if [[ "$__TEST_MODE" == "snapshot" ]]; then
-    temp_dir="$snapshot_dir"
-  fi
+test_setup() {
+  ## Possible values: 'validate', 'snapshot'
+  export __TEST_MODE="${TEST_MODE:-validate}"
+  export __TEST_COMMAND_DIR="${TEST_COMMAND_DIR:-$PWD/tests/commands}"
+  export __TEST_SNAPSHOT_DIR="${TEST_COMMAND_DIR:-$PWD/tests/snapshots}"
+  export __TEST_RESULT_DIR="${TEST_COMMAND_DIR:-$PWD/tests/.results}"
+  export __TEST_TEMPORARY_DIR="${TEST_COMMAND_DIR:-$PWD/tests/.tmp}"
+  export __TEST_INDEX=1
 
-  local name="$*"
-  name="${name// /-}"
-
-  DEBUG=1 KCS_TEST=1 KCS_DIR_COMMANDS="$command_dir" \
-    ./scripts/main.sh "$@" >"$temp_dir/$name.txt" 2>"$temp_dir/$name.log"
-
-  if [[ "$__TEST_MODE" == "validate" ]]; then
-    local expected_file="$snapshot_dir/$name.txt"
-    local actual_file="$temp_dir/$name.txt"
-    _test_result "$name (out)" "$expected_file" "$actual_file"
-
-    expected_file="$snapshot_dir/$name.log"
-    actual_file="$temp_dir/$name.log"
-    _test_result "$name (log)" "$expected_file" "$actual_file"
-  elif [[ "$__TEST_MODE" == "snapshot" ]]; then
-    printf "%-25s: %s\n" \
-      "snapshot $name" "completed"
-  fi
+  __remove_dirs "$__TEST_RESULT_DIR"
+  __create_dirs "$__TEST_SNAPSHOT_DIR" \
+    "$__TEST_RESULT_DIR" \
+    "$__TEST_TEMPORARY_DIR"
 }
 
-_test_result() {
-  local exp act
-  local name="$1" expected="$2" actual="$3"
-  local status="unknown" description=""
+test_cleanup() {
+  __remove_dirs "$__TEST_TEMPORARY_DIR"
 
-  read -r exp _ < <(sha256sum "$expected")
-  read -r act _ < <(sha256sum "$actual")
+  unset __TEST_MODE __TEST_INDEX \
+    __TEST_COMMAND_DIR __TEST_SNAPSHOT_DIR \
+    __TEST_RESULT_DIR __TEST_TEMPORARY_DIR
+}
 
-  if [[ "$exp" != "$act" ]]; then
-    status="failed"
-    description="diff -biy --suppress-common-lines $expected $actual"
-  else
-    status="passed"
+test_case() {
+  local commands=("$@") name
+  name="$(__test_name "${commands[@]}")"
+
+  __test_runner "$name" "${commands[@]}"
+  __test_result "$name"
+
+  ((__TEST_INDEX++))
+}
+
+__test_runner() {
+  local base code out log
+  local name="$1"
+  shift
+
+  base="$__TEST_TEMPORARY_DIR"
+  __test_is_snapshot && base="$__TEST_SNAPSHOT_DIR"
+
+  code="$(__test_filename "$base" "$name" "code")"
+  out="$(__test_filename "$base" "$name" "out")"
+  log="$(__test_filename "$base" "$name" "log")"
+
+  DEBUG=1 KCS_TEST=1 KCS_DIR_COMMANDS="$__TEST_COMMAND_DIR" \
+    ./scripts/main.sh "$@" >"$out" 2>"$log"
+
+  local exit_code="$?"
+  printf "%d" "$exit_code" >"$code"
+
+  return 0
+}
+
+__test_result() {
+  local code out log
+  local name="$1"
+
+  if __test_is_snapshot; then
+    __test_print_result "$name" \
+      "snapshot:completed"
+    return 0
   fi
 
-  printf "%-25s: %s\n" \
-    "$name" "$status"
-  if test -n "$description"; then
-    printf "%-25s: %s\n" \
-      "Description" "$description"
+  local results=()
+
+  ## Verify code
+  results+=("code:$(__test_compare "$name" "code")")
+
+  ## Verify output
+  results+=("output:$(__test_compare "$name" "out")")
+
+  ## Verify logging
+  results+=("logging:$(__test_compare "$name" "log")")
+
+  __test_print_result "$name" \
+    "${results[@]}"
+}
+
+__test_compare() {
+  local name="$1" ttype="$2"
+  local expected actual result
+
+  expected="$(__test_filename "$__TEST_SNAPSHOT_DIR" "$name" "$ttype")"
+  actual="$(__test_filename "$__TEST_TEMPORARY_DIR" "$name" "$ttype")"
+
+  if ! test -f "$expected"; then
+    printf "failed (no-snapshot)"
+    return 0
   fi
+
+  if ! diff -q "$expected" "$actual" >/dev/null; then
+    result="$(__test_filename "$__TEST_RESULT_DIR" "$name-$ttype" "diff")"
+    diff --new-file --suppress-common-lines \
+      --ignore-space-change --ignore-case \
+      --unified "$expected" "$actual" >"$result"
+
+    printf "failed (diff)"
+    return 0
+  fi
+
+  printf "passed"
+  return 0
+}
+
+__test_print_result() {
+  local name="$1"
+  printf "Case #%d: '%s'\n" \
+    "$__TEST_INDEX" "$name"
+
+  shift
+
+  local raw key status
+  for raw in "$@"; do
+    key="${raw%%:*}"
+    status="${raw#*:}"
+
+    printf "%4s%s %-15s: %s\n" \
+      "" "-" "$key" "$status"
+  done
+  echo
+}
+
+__test_name() {
+  local name="$*" separator="-"
+  name="${name// /$separator}"
+  printf "%s" "$name"
+}
+
+## ttype is test type, possible values are 'code', 'out', 'log'
+__test_filename() {
+  local base="$1" name="$2" ttype="$3"
+  printf "%s/%s.%s" "$base" "$name" "$ttype"
+}
+
+__test_is_validate() {
+  [[ "$__TEST_MODE" == "validation" ]] ||
+    [[ "$__TEST_MODE" == "validate" ]] ||
+    [[ "$__TEST_MODE" == "verify" ]] ||
+    [[ "$__TEST_MODE" == "v" ]]
+}
+__test_is_snapshot() {
+  [[ "$__TEST_MODE" == "snapshot" ]] ||
+    [[ "$__TEST_MODE" == "snap" ]] ||
+    [[ "$__TEST_MODE" == "ss" ]] ||
+    [[ "$__TEST_MODE" == "s" ]]
+}
+
+__create_dirs() {
+  local dir
+  for dir in "$@"; do
+    if ! test -d "$dir"; then
+      mkdir -p "$dir"
+    fi
+  done
+}
+
+__remove_dirs() {
+  local dir
+  for dir in "$@"; do
+    if test -d "$dir"; then
+      rm -r "$dir"
+      mkdir -p "$dir"
+      touch "$dir/.gitkeep"
+    fi
+  done
 }
 
 main
