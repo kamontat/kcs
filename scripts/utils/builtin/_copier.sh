8dc3a8b2879a198bf435225f04d65d194f6ca092
Kamontat Chantrachirathumrong <github@kc.in.th>
2023-05-09 13:01:39 +0700

    feat(utils): copier will support cp and rsync and new config

--- scripts/utils/builtin/_copier.sh
+++ scripts/utils/builtin/_copier.sh
@@ -7,6 +7,8 @@
 ## Public functions:
 ##   `kcs_conf_copy_auto_create` - force copy to create missing directory
 ##   `kcs_conf_copy_never_create` - force copy to never missing directory
+##   `kcs_conf_copy_use_cp` - force using 'cp' to copy
+##   `kcs_conf_copy_use_rsync` - force using 'rsync' to copy
 ##   `kcs_copy <base> <target> <name> <new>` - copy file or directory from base
 ##   `kcs_copy_missing <b> <t> <n> <w>` - copy file or directory if missing
 ##   `kcs_copy_lazy <b> <t> <n> <w>` - copy file or directory if needed
@@ -23,6 +25,7 @@ kcs_utils_register "builtin/copier" \
   "builtin/temp" builtin/fs
 
 __KCS_COPIER_AUTO_CREATE=true
+__KCS_COPIER_COMMAND=""
 
 ## make copy will auto create directory on target
 kcs_conf_copy_auto_create() {
@@ -38,6 +41,24 @@ kcs_conf_copy_never_create() {
   __KCS_COPIER_AUTO_CREATE=false
   kcs_debug "$ns" \
     "disable auto create directory"
+}
+
+## force using 'cp' to copy
+kcs_conf_copy_use_cp() {
+  local ns="copy-configure"
+  __KCS_COPIER_COMMAND="cp"
+  kcs_debug "$ns" \
+    "force use '%s' command" \
+    "cp"
+}
+
+## force using 'rsync' to copy
+kcs_conf_copy_use_rsync() {
+  local ns="copy-configure"
+  __KCS_COPIER_COMMAND="rsync"
+  kcs_debug "$ns" \
+    "force use '%s' command" \
+    "rsync"
 }
 
 ## copy file or directory from base
@@ -88,7 +109,6 @@ __kcs_copy() {
   oname="${oname#/*}"
 
   local ipath opath
-
   if test -n "$iname"; then
     ipath="$ibase/$iname"
   fi
@@ -96,10 +116,17 @@ __kcs_copy() {
     opath="$obase/$oname"
   fi
 
+  local test_args=()
   if test -f "$ipath"; then
     ftype="file"
+    test_args+=("-f")
+    "$__KCS_COPIER_AUTO_CREATE" &&
+      kcs_create_file "$opath"
   elif test -d "$ipath"; then
     ftype="directory"
+    test_args+=("-d")
+    "$__KCS_COPIER_AUTO_CREATE" &&
+      kcs_create_dir "$opath"
   fi
 
   kcs_debug "$ns" "starting copy '%s'" "$ftype"
@@ -107,57 +134,117 @@ __kcs_copy() {
     "input: %s, output: %s" \
     "$ipath" "$opath"
 
-  local cmd="cp" cp_args=() test_args=()
-  local shasum no_change
+  test_args+=("$opath")
+  if [[ "$mode" == "missing" ]] &&
+    test "${test_args[@]}"; then
+    kcs_debug "$ns" \
+      "skipping %s: '%s' because it's existed" \
+      "$ftype" "$opath"
+    return 0
+  fi
+
+  if [[ "$__KCS_COPIER_COMMAND" == "rsync" ]]; then
+    __kcs_rsync "$mode" "$ftype" "$ipath" "$opath"
+  elif [[ "$__KCS_COPIER_COMMAND" == "cp" ]]; then
+    __kcs_cp "$mode" "$ftype" "$ipath" "$opath"
+  elif command -v "rsync" >/dev/null; then
+    __kcs_rsync "$mode" "$ftype" "$ipath" "$opath"
+  else
+    __kcs_cp "$mode" "$ftype" "$ipath" "$opath"
+  fi
+}
+
+## Internal cp command with mode supported
+__kcs_cp() {
+  local ns="cp"
+  local mode="$1" ftype="$2"
+  local ipath="$3" opath="$4"
+  test -n "$mode" && ns="$mode-$ns"
+
+  kcs_debug "$ns" \
+    "starting 'cp' command on '%s' mode" \
+    "$mode"
+
+  local cmd="cp" args=()
+
+  local shasum no_change=false
   if [[ "$ftype" == "directory" ]]; then
-    "$__KCS_COPIER_AUTO_CREATE" &&
-      kcs_create_dir "$opath"
-
-    cp_args+=("-r")
-    test_args+=("-d")
-
+    args+=("-r")
     ipath="$ipath/./"
     ## remove double slash if exist
     ipath="${ipath//\/\///}"
 
-    return 0
-  elif [[ "$ftype" == "file" ]]; then
-    test_args+=("-f")
+    local checksum1 checksum2
+    checksum1="$(kcs_temp_create_file)"
+    checksum2="$(kcs_temp_create_file)"
 
-    ## If lazy enabled and same content, return
-    if [[ "$mode" == "lazy" ]]; then
-      local checksum
-      checksum="$(kcs_temp_create_file)"
+    find "$ipath" \
+      -type f \
+      -exec sha256sum "{}" \; |
+      cut -d\  -f1 |
+      sort >"$checksum1"
+    find "$opath" \
+      -type f \
+      -exec sha256sum "{}" \; |
+      cut -d\  -f1 |
+      sort >"$checksum2"
 
-      "$__KCS_COPIER_AUTO_CREATE" &&
-        kcs_create_file "$opath"
-      read -r shasum _ < <(sha256sum "$opath")
-      echo "$shasum $ipath" >"$checksum"
-      if sha256sum --check --status "$checksum"; then
-        no_change=true
-      fi
+    if diff -q "$checksum1" "$checksum2" >/dev/null; then
+      no_change=true
+    fi
+  elif [[ "$ftype" == "file" ]] &&
+    [[ "$mode" == "lazy" ]]; then
+    local checksum
+    checksum="$(kcs_temp_create_file)"
+
+    read -r shasum _ < <(sha256sum "$opath")
+    echo "$shasum $ipath" >"$checksum"
+    if sha256sum --check --status "$checksum"; then
+      no_change=true
     fi
   fi
 
-  test_args+=("$opath")
-  if [[ "$mode" == "missing" ]] &&
-    test "${test_args[@]}"; then
-    no_change=true
-  fi
-
-  if test -n "$no_change"; then
+  if "$no_change"; then
     kcs_debug "$ns" \
-      "no changes on file (%s), exit" "$opath"
+      "skipping %s: '%s' because same content" \
+      "$ftype" "$opath"
     return 0
   fi
 
-  cp_args+=(
-    "$ipath"
-    "$opath"
+  args+=("$ipath" "$opath")
+  kcs_exec "$cmd" "${args[@]}"
+}
+
+## Internal rsync command with mode supported
+__kcs_rsync() {
+  local ns="rsync"
+  local mode="$1" ftype="$2"
+  local ipath="$3" opath="$4"
+  test -n "$mode" && ns="$mode-$ns"
+
+  kcs_debug "$ns" \
+    "starting 'rsync' command on '%s' mode" \
+    "$mode"
+
+  local cmd="rsync" args=()
+
+  args+=(
+    --sparse --times
+    --executability --perms
+    --quiet
   )
 
-  kcs_info "$ns" \
-    "copying '%s' to '%s'" \
-    "$ipath" "$opath"
-  kcs_exec "$cmd" "${cp_args[@]}"
+  if [[ "$mode" == "lazy" ]]; then
+    args+=(--checksum)
+  fi
+
+  args+=("$ipath" "$opath")
+  kcs_exec "$cmd" "${args[@]}"
+}
+
+kcs_add_hook clean \
+  __kcs_copier_clean
+__kcs_copier_clean() {
+  unset __KCS_COPIER_COMMAND \
+    __KCS_COPIER_AUTO_CREATE
 }
