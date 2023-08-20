56de70603592e551175e23fe715563eee2928616
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2023-08-20 20:52:06 +0700

    perf(scripts): update setup with new design

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,36 +2,114 @@
 
 set -e
 
-__INSTALL_DIR="${1:-$PWD}"
+main() {
+  local source="$1" entrypoint="$2" target="$3"
 
-! command -v git >/dev/null &&
-  echo "git command is missing" >&2 &&
+  test -d "${source:?}" ||
+    _error "source directory '%s' is missing" "$source"
+  test -d "${target:?}" ||
+    _error "target directory '%s' is missing" "$target"
+  command -v git >/dev/null ||
+    _error "command '%s' is missing" "git"
+
+  _info "%-15s %-18s | %s" "Installing..." "kcs" "$target"
+
+  local scripts="$target/scripts"
+
+  _delete "scripts" "$scripts"
+  _create_dir "scripts" "$scripts"
+
+  _move "source code" "$source/src" "$scripts/.kcs"
+  _move "kcs version" "$source/version.txt" "$scripts/.kcs/version.txt"
+  _create_script "index.sh" "$scripts/index.sh" "$entrypoint"
+
+  _create_dir "commands" "$scripts/commands"
+  _move "default command" \
+    "$scripts/.kcs/commands/_default.sh" "$scripts/commands/_default.sh"
+
+  return 0
+}
+
+_create_script() {
+  local name="$1" target="$2" content="$3"
+  _info "%-15s %-18s" "Creating..." "$name"
+  echo "$content" >"$target" ||
+    _error "cannot create '%s' file" "$target"
+  chmod +x "$target" ||
+    _error "cannot grant permission to '%s'" "$target"
+}
+_create_dir() {
+  local name="$1" target="$2"
+  _info "%-15s %-18s" "Creating..." "$name"
+  mkdir -p "$target" ||
+    _error "cannot create '%s' directory" "$target"
+}
+_copy() {
+  local name="$1" source="$2" target="$3"
+  _info "%-15s %-18s" "Copying..." "$name"
+  cp -r "$source" "$target" ||
+    _error "cannot move '%s' to '%s'" "$source" "$target"
+}
+_move() {
+  local name="$1" source="$2" target="$3"
+  _info "%-15s %-18s" "Moving..." "$name"
+  mv "$source" "$target" ||
+    _error "cannot move '%s' to '%s'" "$source" "$target"
+}
+_delete() {
+  local name="$1" target="$2"
+  if test -d "$target" || test -f "$target"; then
+    _info "%-15s %-18s" "Deleting..." "$name"
+    rm -rf "$target" ||
+      _error "cannot delete '%s'" "$target"
+  fi
+}
+
+_debug() {
+  local format="$1"
+  shift
+  printf "[%s] $format\n" "DBG" "$@"
+}
+_info() {
+  local format="$1"
+  shift
+  printf "[%s] $format\n" "INF" "$@"
+}
+_error() {
+  local format="$1"
+  shift
+  printf "[%s] $format\n" "ERR" "$@" >&2
   exit 1
+}
 
-__REPO="https://github.com/kc-workspace/kcs.git"
-__TEMP="$(mktemp -d)"
-__ENTRYPOINT="#!/usr/bin/env bash
+__internal() {
+  local cmd="$1"
+  shift
 
-if [ -f \"\$(dirname \"\$0\")/.kcs/main.sh\" ]; then
-  \"\$(dirname \"\$0\")/.kcs/main.sh\" \"\$@\"
-else
-  echo \"cannot found main.sh file\" >&2
-  exit 1
-fi"
+  local current="$PWD"
+  local https="https://github.com/kc-workspace/kcs.git"
+  local ssh="git@github.com:kc-workspace/kcs.git"
 
-echo "On $__INSTALL_DIR, installing..."
+  local source
+  source="$(mktemp -d)"
+  ## delete source because initiate will create source instead
+  test -d "$source" && rm -r "$source"
 
-test -d "$__INSTALL_DIR/scripts/.kcs" &&
-  rm -r "$__INSTALL_DIR/scripts/.kcs"
-mkdir -p "$__INSTALL_DIR/scripts" &&
-  git clone "$__REPO" \
-    --branch "main" --single-branch \
-    "$__TEMP" &&
-  mv "$__TEMP/src" "$__INSTALL_DIR/scripts/.kcs" &&
-  echo "$__ENTRYPOINT" >"$__INSTALL_DIR/scripts/kcs" &&
-  chmod +x "$__INSTALL_DIR/scripts/kcs" &&
-  rm -rf "$__TEMP"
+  local remote
+  remote="$(git -C "$current" remote get-url origin)"
+  if [[ "$remote" == "$ssh" ]] || [[ "$remote" == "$https" ]]; then
+    cp -r "$current" "$source"
+  else
+    git clone "$https" --branch "main" --single-branch "$source"
+  fi
 
-unset __INSTALL_DIR
-unset __REPO __TEMP
-unset __ENTRYPOINT
+  local entrypoint="#!/usr/bin/env bash
+entrypoint=\"\$(dirname \"\$0\")/.kcs/main.sh\"
+[ -f \"\$entrypoint\" ] && \"\$entrypoint\" \"\$@\"
+"
+
+  "$cmd" "$source" "$entrypoint" "$@" &&
+    rm -fr "$source"
+}
+
+__internal main "$@"
